213e39cf414ca83cd32b210bfcc5278e6664bc55
Sean Harvey <halkyon@gmail.com>
2020-03-17 04:38:07 +0000

    day 2 part 2

--- day2/main.go
+++ day2/main.go
@@ -16,35 +16,56 @@ const (
 )
 
 func main() {
-	var nums []int
+	nums := make(map[int]int)
 	input, err := ioutil.ReadAll(os.Stdin)
 	if err != nil {
 		log.Fatal(err)
 	}
-	for _, num := range strings.Split(string(input), ",") {
+	for i, num := range strings.Split(string(input), ",") {
 		num, err := strconv.Atoi(num)
 		if err != nil {
 			log.Fatal(err)
 		}
-		nums = append(nums, num)
+		nums[i] = num
 	}
-	for i, num := range nums {
-		if i%4 != 0 {
-			continue
+
+	// part 1
+	num := calculate(nums[1], nums[2], nums)
+	fmt.Println(num)
+
+	// part 2
+	for i := 0; i < 100; i++ {
+		for j := 0; j < 100; j++ {
+			if calculate(i, j, nums) == 19690720 {
+				fmt.Println(100*i + j)
+				break
+			}
 		}
-		src1 := nums[nums[i+1]]
-		src2 := nums[nums[i+2]]
-		destIdx := nums[i+3]
+	}
+}
+
+func calculate(num1 int, num2 int, nums map[int]int) int {
+	// make a copy of the map so we don't cause side-effects to the original
+	tmp := make(map[int]int)
+	for i, v := range nums {
+		tmp[i] = v
+	}
+
+	tmp[1] = num1
+	tmp[2] = num2
+	idx := 0
+	for tmp[idx] != 99 {
+		num := tmp[idx]
+		src1 := tmp[tmp[idx+1]]
+		src2 := tmp[tmp[idx+2]]
+		destIdx := tmp[idx+3]
 		switch num {
 		case opAdd:
-			nums[destIdx] = src1 + src2
+			tmp[destIdx] = src1 + src2
 		case opMult:
-			nums[destIdx] = src1 * src2
-		case opHalt:
-			fmt.Printf("%+v\n", nums)
-			return
-		default:
-			log.Fatalf("cannot handle opcode %d\n", num)
+			tmp[destIdx] = src1 * src2
 		}
+		idx += 4
 	}
+	return tmp[0]
 }
